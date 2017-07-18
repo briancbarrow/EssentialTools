515fa3e59793dbfb83372a36e947aabb90592d6c
Brian Barrow <briancbarrow@gmail.com>
2017-07-18 17:45:30 -0600

    Added Ninject

---
+++ EssentialTools/Models/IValueCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class IValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+}
